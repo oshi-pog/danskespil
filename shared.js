c4fa83c09740a3d6e1995bca355150f69702ce9a
Molnar Adam <molnaradam@Molnars-MacBook-Pro.local>
2019-05-25 22:50:36 +0200

    odometer

--- shared.js
+++ shared.js
@@ -2,8 +2,10 @@
 
 //Variables
 const burgerIcon = document.querySelector(".bottom-menu-middle");
+const bottomMenuOverlay = document.querySelector("#bottom-menu-overlay");
 const bottomMenuContent = document.querySelector(".bottom-menu-content");
 let menuStatus = burgerIcon.dataset.status;
+let bottomMenuOverlayHeight = null;
 
 window.addEventListener("DOMContentLoaded", () => {
   //Add event listener to burger icon and run function to open or close it based on it's current status
@@ -15,16 +17,29 @@ window.addEventListener("DOMContentLoaded", () => {
       closeOverlayMenu();
     }
   });
+  runOdometer();
 });
 
 function openOverlayMenu() {
   bottomMenuContent.style.height = "100%";
+  bottomMenuOverlay.style.height = "80%";
   menuStatus = "open";
   console.log(menuStatus);
 }
 
 function closeOverlayMenu() {
   bottomMenuContent.style.height = "0";
+
+  setTimeout(() => {
+    bottomMenuOverlay.style.height = "0";
+  }, 300);
   menuStatus = "closed";
   console.log(menuStatus);
+}
+
+//COUNTER
+function runOdometer() {
+  setTimeout(() => {
+    odometer.innerHTML = 43600000;
+  }, 1000);
 }
