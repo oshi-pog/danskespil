b955f2e5aa308ae3e06937f53ce8b57ca76a661b
adeekahh <molnaradam@envy13.kea.local>
2019-06-04 17:23:24 +0200

    players.html

---
+++ menu.js
@@ -0,0 +1,7 @@
+const burgerIcon = document.querySelector(".bottom-menu-middle");
+const bottomMenuOverlay = document.querySelector("#bottom-menu-overlay");
+const bottomMenuContent = document.querySelector(".bottom-menu-content");
+const bottomOverlay = document.querySelector("#bottom-menu-overlay");
+
+let menuStatus = burgerIcon.dataset.status;
+let bottomMenuOverlayHeight = null;
